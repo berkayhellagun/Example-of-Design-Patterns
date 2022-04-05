a5a72adfcc13a239f531333d2eec793c19ea952c
berkayhellagun <berkayhellagun@hotmail.com>
2022-04-05 16:14:40 +0300

    Add project files.

--- Files of Work/SOLIDenemeler/Program.cs
+++ Files of Work/SOLIDenemeler/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
 public class Program
-{ 
-    
-    static void Main(string [] args)
+{
+
+    static void Main(string[] args)
     {
         generic tot = new generic(new DBFinder(), new Web());
         tot.bas("ağaç");
+
+        Dictionary<string, string> keys = new Dictionary<string, string>();
+        keys.Add("london", "england");
+        var deger = keys["london"];
+        System.Console.WriteLine(deger);
     }
 }
 
@@ -14,7 +19,7 @@ public class generic
 {
     IFinder _finder;
     IRenderer _render;
-    public generic(IFinder finder,IRenderer renderer)
+    public generic(IFinder finder, IRenderer renderer)
     {
         _finder = finder;
         _render = renderer;
@@ -51,14 +56,14 @@ public class Console : IRenderer
 {
     public void Display(string content)
     {
-        System.Console.WriteLine(content +"konsola basıldı");
+        System.Console.WriteLine(content + "konsola basıldı");
     }
 }
 public class Web : IRenderer
 {
     public void Display(string content)
     {
-        System.Console.WriteLine(content +"webe yazıldı");
+        System.Console.WriteLine(content + "webe yazıldı");
     }
 }
 
